bae1e5b090e4ad116a64787190e718f48fbf5825
JosueDurand96 <josuedurandf@gmail.com>
2022-10-12 05:23:10 -0500

    manejo de errores enum

--- app/src/main/java/com/durand/dogedex/ui/doglist/DogListActivity.kt
+++ app/src/main/java/com/durand/dogedex/ui/doglist/DogListActivity.kt
@@ -3,8 +3,11 @@ package com.durand.dogedex.ui.doglist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.durand.dogedex.api.ApiResponseStatus
 import com.durand.dogedex.databinding.ActivityDogListBinding
 import com.durand.dogedex.ui.dogdetail.DogDetailActivity
 import com.durand.dogedex.ui.dogdetail.DogDetailActivity.Companion.DOG_KEY
@@ -18,8 +21,7 @@ class DogListActivity : AppCompatActivity() {
         val binding = ActivityDogListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        // val dogList = getFakeDogs()
+        val loadingWheel = binding.loadingWheel
         val recycler = binding.dogRecycler
         recycler.layoutManager = LinearLayoutManager(this)
         val adapter = DogAdapter()
@@ -34,6 +36,29 @@ class DogListActivity : AppCompatActivity() {
             adapter.submitList(dogList)
         }
 
+        dogListViewModel.status.observe(this) {
+                status ->
+                when (status) {
+                    ApiResponseStatus.LOADING -> {
+                     binding.loadingWheel.visibility = View.VISIBLE
+                    }
+                    ApiResponseStatus.ERROR -> {
+                        binding.loadingWheel.visibility = View.GONE
+                        Toast.makeText(this,"Error al cargar los datos!", Toast.LENGTH_SHORT).show()
+                    }
+                    ApiResponseStatus.SUCCESS -> {
+                        binding.loadingWheel.visibility = View.GONE
+                        Toast.makeText(this,"Se cargaron los datos correctamente!", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> {
+                        binding.loadingWheel.visibility = View.GONE
+                        Toast.makeText(this,"Ocurrio un error!", Toast.LENGTH_SHORT).show()
+                    }
+
+
+                }
+
+        }
 
     }
 
