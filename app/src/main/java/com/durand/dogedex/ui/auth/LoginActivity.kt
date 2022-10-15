82d491a57ff79f2e56402560ed435ba347800bb4
JosueDurand96 <josuedurandf@gmail.com>
2022-10-15 15:16:14 -0500

    login screen

--- app/src/main/java/com/durand/dogedex/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/durand/dogedex/ui/auth/LoginActivity.kt
@@ -2,11 +2,12 @@ package com.durand.dogedex.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.durand.dogedex.R
+import com.durand.dogedex.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
