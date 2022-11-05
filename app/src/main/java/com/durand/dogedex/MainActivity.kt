732599bcfba60c6d69ccaac707c1a329da482249
JosueDurand96 <josuedurandf@gmail.com>
2022-11-05 13:35:57 -0500

    take photo

--- app/src/main/java/com/durand/dogedex/MainActivity.kt
+++ app/src/main/java/com/durand/dogedex/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.durand.dogedex
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.durand.dogedex.api.User
 import com.durand.dogedex.databinding.ActivityMainBinding
 import com.durand.dogedex.ui.auth.LoginActivity
@@ -14,6 +23,17 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                startCamera()
+            } else {
+                Toast.makeText(this, R.string.camera_permission, Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,8 +43,8 @@ class MainActivity : AppCompatActivity() {
         if (user == null) {
             openLoginActivity()
             return
-        }else{
-            Log.d("josue","authenticationToken: "+user.authenticationToken)
+        } else {
+            Log.d("josue", "authenticationToken: " + user.authenticationToken)
             ApiServiceInterceptor.setSessionToken(user.authenticationToken)
         }
 
@@ -32,8 +52,9 @@ class MainActivity : AppCompatActivity() {
             openSettingsActivity()
         }
         binding.dogListFab.setOnClickListener {
-            startActivity(Intent(this,DogListActivity::class.java))
+            startActivity(Intent(this, DogListActivity::class.java))
         }
+        requestCameraPermission()
     }
 
     private fun openSettingsActivity() {
@@ -46,5 +67,53 @@ class MainActivity : AppCompatActivity() {
         finish()
     }
 
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                startCamera()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                AlertDialog.Builder(this)
+                    .setTitle("Aceptame por favor")
+                    .setMessage("Acepta la camara o me da ansiedad xD")
+                    .setPositiveButton(android.R.string.ok) { _, _ ->
+                        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+
+                    }
+                    .setNegativeButton(android.R.string.cancel) { _, _ ->
+                    }
+                    .show()
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.CAMERA
+                )
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(
+            {
+                val cameraProvider = cameraProviderFuture.get()
+
+                val preview = Preview.Builder().build()
+                preview.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
+
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector,
+                    preview
+                )
+            }, ContextCompat.getMainExecutor(this)
+        )
+    }
 
 }
