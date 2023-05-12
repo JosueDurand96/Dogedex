07ee894f5dda8e64205fc527508e3d7b583bf90b
Josué Durand Pebe <josue.durand8@gmail.com>
2023-05-12 13:46:29 -0500

    ReporteCanesRegistradosFragment

--- app/src/main/java/com/durand/dogedex/ui/admin_fragment/ui/reporte_canes_registrados/ReporteCanesRegistradosFragment.kt
+++ app/src/main/java/com/durand/dogedex/ui/admin_fragment/ui/reporte_canes_registrados/ReporteCanesRegistradosFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.durand.dogedex.databinding.FragmentReportRegisterCanBinding
 
 class ReporteCanesRegistradosFragment : Fragment() {
@@ -25,6 +26,23 @@ class ReporteCanesRegistradosFragment : Fragment() {
         _binding = FragmentReportRegisterCanBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // this creates a vertical layout Manager
+        _binding!!.canReportLostRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemsViewModelReporte>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ItemsViewModelReporte("ss", "Item " + i))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = ReporteCanesRegistradosAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        _binding!!.canReportLostRecyclerView.adapter = adapter
 
         return root
     }
